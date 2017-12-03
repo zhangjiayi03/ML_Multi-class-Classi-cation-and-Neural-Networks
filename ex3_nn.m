17e3ad6cef2f9d3338cac97bde8e898e2f83d305
zhangjiayi03 <zjy_work1@163.com>
2017-12-03 13:29:32 +0800

    first change

--- ex3_nn.m
+++ ex3_nn.m
@@ -17,7 +17,9 @@
 %
 
 %% Initialization
-clear ; close all; clc
+clear ; 
+close all; 
+clc
 
 %% Setup the parameters you will use for this exercise
 input_layer_size  = 400;  % 20x20 Input Images of Digits
